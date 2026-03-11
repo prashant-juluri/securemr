dd62ac5f8d07cac9836ee8ecdce9e112df846f54
Prashant Juluri <prashantjuluri@192.168.1.3>
2026-03-11 13:02:54 +0530

    Update Semgrep ruleset to OWASP Top 10

--- scanners/semgrep_runner.py
+++ scanners/semgrep_runner.py
@@ -10,13 +10,14 @@ def run_semgrep(target_path="."):
     output_path = tmp_file.name
 
     cmd = [
-    "semgrep",
-    "--config=p/security-audit",
-    "--no-git-ignore",
-    target_path,
-    "--json",
-    "--output",
-    output_path
+        "semgrep",
+        "--config=p/owasp-top-ten",
+        "--no-git-ignore",
+        "--metrics=off",
+        target_path,
+        "--json",
+        "--output",
+        output_path
     ]
 
     print(f"[SecureMR] Executing Semgrep on {target_path}")

7f40dfe9df56c9afcb5454abb32c3344c05ea977
Prashant Juluri <prashantjuluri@192.168.1.3>
2026-03-14 16:50:06 +0530

    github MR reporter v1.0

--- context/diff_analyzer.py
+++ context/diff_analyzer.py
@@ -3,22 +3,6 @@ from os import path
 import subprocess
 import sqlite3
 
-
-def search_users(user_input):
-
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
-
-    query = "SELECT * FROM users WHERE name = '" + user_input + "'"
-
-    cursor.execute(query)
-
-
-
-    
-
-    return cursor.fetchall()
-
 def get_repo_root():
     """
     Detect the Git repository root inside the container.
